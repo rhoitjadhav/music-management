278ef267979573e1839f995d64394baeb2473a78
rhoitjadhav <jadhavrohit497@gmail.com>
2020-09-20 23:17:41 +0530

    blueprint, and index route added

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from flask.helpers import url_for
 from werkzeug.utils import redirect
 from routes import songs
-from flask import Flask, redirect
+from flask import Flask, redirect, render_template
 
 app = Flask(__name__)
 app.config.from_json("config/config.json")
+
+
+@app.route("/")
+def index():
+    return redirect(url_for('songs.view'))
+
+
+# Blueprints
+app.register_blueprint(songs.bp)
+
 
 if __name__ == '__main__':
     host = app.config['HOST']
